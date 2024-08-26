19a8bd39a09e56a68882d41427849454435ed4b5
WFJansenvanVuuren <wfjansenvanvuuren@outlook.com>
2024-08-26 22:49:04 +0200

    update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -19,7 +19,7 @@ let myCurrentJob = "Draughtsman"
 
 
 
-Data Types:
+//                          Data Types:
 
 // The first time we declare a variable we need to use let
 
@@ -36,7 +36,8 @@ javascriptIsFun = 'YES!'
 console.log(typeof javascriptIsFun);
 console.log(javascriptIsFun);
 
-// Undefined:
+
+//                          Undefined:
 let year;
 console.log(year);
 console.log(typeof year);
@@ -53,12 +54,12 @@ const birthYear = 1991;
 
 var job = 'programmer'
 job = 'teacher'
-*/
 
-// Arithmetic/Math Operators
-const now = 2024;
-const ageJonas = now - 1989;
-const ageSarah = now - 2015;
+
+// Arithmetic/Math Operators:
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
@@ -67,7 +68,8 @@ const firstName = 'Joe'
 const lastName = 'Doe'
 console.log(firstName + ' ' + lastName);
 
-// Assignment Operators
+
+//                  Assignment Operators:
 let x = 10 + 5;
 x += 10; // x = x + 10 = 25
 x *= 4; // x = x * 4 = 100
@@ -82,4 +84,63 @@ console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
 
-console.log(now - 1989 > now - 2015);
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1989 > now - 2018);
+
+console.log(25-10-5);
+
+let x, y;
+x = y = 25-10-5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas,ageSarah, averageAge);
+
+
+//                      Challenge #1: (BMI Calculator)
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+//                  Strings and Template Literals:
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+
+console.log(`Just a regular string...`);
+
+// Old Way:
+console.log('String with \n\
+multiple \n\
+lines');
+
+//New Way:
+
+console.log(`String with
+multiple
+lines`);
+*/
