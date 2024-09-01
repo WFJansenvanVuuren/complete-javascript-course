572f24f5a409060e71eceb49fa96cebc6f5939a8
WF Jansen van Vuuren <wfjansenvanvuuren@outlook.com>
2024-09-01 21:46:10 +0200

    Completed Fundentals Part 1
    Completed Coding Challenge 4

--- 01-Fundamentals-Part-1/starter/coding challenges.js
+++ 01-Fundamentals-Part-1/starter/coding challenges.js
@@ -65,5 +65,7 @@ const bill = 430;
 //     tip = bill * 0.20
 // }
 
-console.log(`The bill was ${bill}, the tip was ${tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill}, and the total value ${bill + tip}.`);
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`);
 
