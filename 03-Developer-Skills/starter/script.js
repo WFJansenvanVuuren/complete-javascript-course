070d556cb111b9fc1a33744ff622d0eabe1abed8
WF Jansen van Vuuren <wfjansenvanvuuren@outlook.com>
2024-09-19 20:56:48 +0200

    Update to script.js
    Completed debugger Lecture

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 //  PROBLEM 1:
 // Given an array of tempretures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error.
 
@@ -62,3 +63,53 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+// Debugging
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // value: Number(prompt('Degrees celsius:')), //C: FIX THE BUG - CONVERT STRING TO A NUMBER
+    value: 10,
+  };
+
+  // B: FIND THE BUG
+  // How to show objects in the console:
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A: IDENTIFY THE BUG
+console.log(measureKelvin());
+
+//HOW TO USE A DEBUGGER
+
+//Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
