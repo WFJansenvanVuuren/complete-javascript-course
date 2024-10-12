44c976484ea7c9386e892f29bfe459b1a85ca631
WF Jansen van Vuuren <wfjansenvanvuuren@outlook.com>
2024-10-12 14:16:13 +0200

    completed Pig-Game

--- 02-Fundamentals-Part-2/starter/coding-challenges.js
+++ 02-Fundamentals-Part-2/starter/coding-challenges.js
@@ -1,23 +1,18 @@
-'use strict';
+"use strict";
 
-/*
 //                        Coding Challenge 1:
 
-const calcAverage = scores => scores / 3;
+const calcAverage = (scores) => scores / 3;
 
-
-
-function checkWinner (avgDolphins, avgKoalas){
-
+function checkWinner(avgDolphins, avgKoalas) {
   const scoreDolphins = calcAverage(avgDolphins);
   const scoreKoalas = calcAverage(avgKoalas);
-    
+
   if (avgDolphins >= 2 * avgKoalas) {
-  return `Dolphins win (${scoreDolphins} vs ${scoreKoalas})`;
+    return `Dolphins win (${scoreDolphins} vs ${scoreKoalas})`;
   } else if (scoreKoalas >= 2 * scoreDolphins) {
-  return `Koalas win (${scoreKoalas} vs ${scoreDolphins})`;
-  } else 
-  return 'No team wins...'
+    return `Koalas win (${scoreKoalas} vs ${scoreDolphins})`;
+  } else return "No team wins...";
 }
 console.log(checkWinner([44 + 23 + 71], [65 + 54 + 49]));
 
@@ -29,15 +24,15 @@ let scoreDolphins = calcAverage(44, 23, 71);
 let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
 
-const checkWinner = function (avgDolphins, avgKoalas){
+const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`); 
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas >= 2 * avgDolphins) {
     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
-    console.log('No team wins...')
+    console.log("No team wins...");
   }
-}
+};
 
 checkWinner(scoreDolphins, scoreKoalas);
 
@@ -48,90 +43,81 @@ scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
 
-
 //                        Coding Challenge 2:
 
 const calcTip = function (bill) {
-return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
-}
+  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+};
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
 
-
-
-//                        Coding Challenge 3:  
+//                        Coding Challenge 3:
 
 const mark = {
-  fullName: 'Mark Miller',
+  fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
-  calcBMI: function() {
+  calcBMI: function () {
     this.bmi = this.mass / this.height ** 2;
     return this.bmi;
-  }
+  },
 };
 
 const john = {
-  fullName: 'John Smith',
+  fullName: "John Smith",
   mass: 92,
   height: 1.95,
-  calcBMI: function() {
+  calcBMI: function () {
     this.bmi = this.mass / this.height ** 2;
     return this.bmi;
-  }
+  },
 };
 
 john.calcBMI();
 mark.calcBMI();
 
 if (john.bmi > mark.bmi) {
-  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi}) BMI!`);
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi}) BMI!`
+  );
 } else {
-  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi}) BMI!`);
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi}) BMI!`
+  );
 }
-
-*/
 
 //                        Coding Challenge 4:
 
 const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
-  }
+  return bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+};
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
 const tips = [];
 const totals = [];
-  
-for (let i = 0; i < bills.length; i ++) { 
+
+for (let i = 0; i < bills.length; i++) {
   const tip = calcTip(bills[i]);
-  tips.push(tip);  
-  totals.push(tip + bills[i])
-} 
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
 
 console.log(bills, tips, totals);
 
-const calcAverage = function(arr) {
+const calcAverage = function (arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     // sum = sum + arr[i];
     sum += arr[i];
   }
-  return sum / arr.length
-}
+  return sum / arr.length;
+};
 console.log(calcAverage(totals));
 console.log(calcAverage(tips));
 
-
-
-
-
-
-
 // const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 // const totals = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]
-
-
--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,92 @@
 'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+//We can use another method 👇 to select Id's using getElementById
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //1. Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    //3. Check for rolled 1: if true,
+    if (dice !== 1) {
+      //Add to score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //3. Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current score to the score of the active player.
+    scores[activePlayer] += currentScore;
+    //eg. scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //2. Check if score is >= 100,
+    if (scores[activePlayer] >= 20) {
+      // Finish game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      //switch to the next player.
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  document.getElementById('score--0').textContent = 0;
+  document.getElementById('score--1').textContent = 0;
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+});
