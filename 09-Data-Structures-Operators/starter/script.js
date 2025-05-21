07ea7a52747ae6b28e4500ea92c0a0212c76a779
WF Jansen van Vuuren <wfjansenvanvuuren@outlook.com>
2025-05-22 00:59:09 +0200

    upload

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -27,7 +27,7 @@ for (const flight of flights.split('+')) {
 //   console.log('Part of the new Airbus family');
 // }
 
-/*
+
 // Data needed for first part of the section
 //ES6 Enchanced Object Literals
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -1122,6 +1122,143 @@ const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
 //Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
+//OLD
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//NEW
+
+const restaurant = {
+  restaurantName: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, //Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(starterIndex, mainIndex, time, address);
+  },
+};
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sol',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//Destructuring Objects
+
+//When destructuting the variable names need to match the propierty names:
+const { restaurantName, openingHours, categories } = restaurant;
+console.log(restaurantName, openingHours, categories);
+
+//To change the variable name:
+const {
+  restaurantName: newName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(newName, hours, tags);
+
+//Default values:(Usefull when you do not know the names of data given)
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+console.log(a, b);
+//You cannot start a line with curly braces, javascript think you want to start a code block.
+//We then wrap it in perentheces.
+({ a, b } = obj);
+
+console.log(a, b);
+
+//Nested objects
+//Object inside an object, inside an object, inside an onject
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
+
+//We can change the variable name
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
+////////////////////////////////////////////////////////////////////////
+//Destructuring an array
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//With destructuring we can declare all the variables at the same time.
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// const [first, second] = restaurant.categories;
+//Let say we want to take the first and the third item from the array.
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Swithing variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+//Function return an array and immediately destruct into variables.
+//Getting multiple values from a function.
+
+//Receive to return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Destructuring a nested array
+//We do destructuring inside of destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//We can set default values for the variables when extracting them.
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
 */
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -695,7 +695,7 @@ console.log(
   } movements ago`
 );
 */
-
+/* 
 //////////////////////////////////////////////////
 //The some & every Methods:
 
@@ -708,12 +708,116 @@ console.log(movements);
 //Checks for Equality .Returns a boolean
 console.log(movements.includes(-130)); //Test if it includes a certain value.
 
-//The some Methods:
-
+//The some Method:
 //Checks for a Condition .Returns a boolean
 console.log(movements.some(mov => mov === -130)); //Here we would rather use includes method.
 
-const anyDesposits = movements.some(mov => mov > 1500); //Returns a boolean
+const anyDesposits = movements.some(mov => mov > 0); //Returns a boolean
 console.log(anyDesposits);
 
-//The every Methods:
+//The every Method:
+//every only returns true if all the elements in the array meet the condition we passed in.
+
+console.log(movements.every(mov => mov > 0));
+
+//Separate Call Back
+//This way we can reuse the same function for different methods.
+const deposit = mov => mov > 0;
+
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+*/
+
+//////////////////////////////////////////////////
+//The flat & flatMap Methods:
+
+//flat Method:
+//Removes nested arrays and flattens the array
+//For working with nested arrays.
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+//flat Method only goes one level deep when flattening the array
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat());
+
+console.log(arrDeep.flat(2)); //We can fix this when we go 2 levels deep
+
+//Getting all the accounts movements and adding them together:
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+//We can do this with chainng:
+const overallBalance2 = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
+
+//To use map and then flat is a very common method so they create the flatMap Method.
+
+//flatMap Method:
+//flatMap can only go one level deep.
+const overallBalance3 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance3);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+dogs.forEach(function (dog) {
+  dog.recommendedFood = dog.weight ** 0.75 * 28 * 0.1;
+});
+console.log(dogs);
+
+//2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose)
+//const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+
+const sarahDog = dogs.flatMap(dogs => dogs.owners);
+find(dog => dog.owners === 'Sarah');
+console.log(dogs.flatMap(dogs => dogs.owners));
+
+console.log(sarahDog);
